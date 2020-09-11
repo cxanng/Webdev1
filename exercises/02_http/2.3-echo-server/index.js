1ee60fefc4a2628f581ab55b561464e3b022f292
Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-09-11 14:14:06 +0300

    test ex 2.3

---
+++ exercises/02_http/2.3-echo-server/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    let body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+    });
+    res.writeHead(200, {'Context-Type': 'text/plain'});
+    res.write(JSON.stringify(body));
+    res.end();
+})
+
+// server.listen(3000, '127.0.0.1');
+// console.log('listening');
+
