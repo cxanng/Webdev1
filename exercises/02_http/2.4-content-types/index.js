3592431e0840cd8068a6e5eb2da5cea25e3edb8e
Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-09-11 22:37:34 +0300

    test ex 2.4

---
+++ exercises/02_http/2.4-content-types/index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    if (req.headers.accept  === 'text/xml' || 
+    req.headers.accept === '*/*') {
+        fs.readFile('data.html', (err, data) => {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.write(data);
+            res.end();
+        });
+        
+    }
+    else if (req.headers.accept === 'aplication/json') {
+        fs.readFile('data.json', (err, data) => {
+            res.writeHead(200, {'Content-Type' : res.headers.accept});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.headers.accept === 'text.xml') {
+        fs.readFile('data.xml', (err, data) => {
+            res.writeHead(200, {'Content-Type' : res.headers.accept});
+            res.write(data);
+            res.end();
+        })
+    }
+    else {
+        res.statusCode = 406;
+        res.statusMessage = "Content type not available";
+        res.end();
+    }
+})
+
+server.listen(3000, '127.0.0.1');
+console.log('listening');
