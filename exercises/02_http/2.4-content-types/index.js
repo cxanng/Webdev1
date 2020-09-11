b50873199441da35df1f490cee1a90ea7f26941e
Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-09-11 23:14:30 +0300

    test 3 ex 2.4

--- exercises/02_http/2.4-content-types/index.js
+++ exercises/02_http/2.4-content-types/index.js
@@ -1,14 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const possibleHeaders = {
+    "text/xml" : "data.xml",
+    "text/html" : "data.txt",
+    "aplication/json" : "data.json",
+    "*/*" : "data.txt"
+}
+
 const server = http.createServer((req, res) => {
     if (req.headers.accept  === "text/xml" || 
     req.headers.accept === "*/*") {
-        fs.readFile('data.html', (err, data) => {
-            res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.readFile("data.html", (err, data) => {
+            res.writeHead(200, {'Content-Type' : "text/html"});
             res.write(data);
             res.end();
-        });
-        
+        });        
     }
     else if (req.headers.accept === "aplication/json") {
         fs.readFile('data.json', (err, data) => {
