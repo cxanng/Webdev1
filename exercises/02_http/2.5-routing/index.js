334b455bbed9dba1ce1e6d81f886b8df72981f03
Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-09-12 00:00:36 +0300

    test ex 2.5

---
+++ exercises/02_http/2.5-routing/index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const pathLead = {
+    "/" : "index.html",
+    "/classical" : "homer.html",
+    "/modern" : "bradbury.html"
+}
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req;
+    if (Object.keys(pathLead).includes(url)) {
+        fs.readFile(pathLead[url], (err, data) =>{
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else {
+        res.writeHead(404, 'Requested content not found');
+        res.end();
+    }
+})
+
+server.listen(3000, '127.0.0.1');
