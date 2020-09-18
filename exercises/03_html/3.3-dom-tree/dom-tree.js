2635153a18a88fcf2e5ee99a7d7bcfb61e0c5052
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-09-18 16:16:05 +0300

    test 2 ex 3.3

--- exercises/03_html/3.3-dom-tree/dom-tree.js
+++ exercises/03_html/3.3-dom-tree/dom-tree.js
@@ -1,18 +1,20 @@
-// Add an unordered list to the navigation link list
-const newList = document.createElement('ul');
-const parentDiv = document.getElementByTagName("nav")[0].appendChild(newList);
+// Add a class list to the navigation class list
+const parentDiv = document.getElementByClassName("navi");
+parentDiv.classList.add('list');
 
 // Add a link to the nav link list
-const newItem = document.createElement('li');
-const newLink = document.createElement('a');
-newLink.innerText = "Localhost";
+var newItem = document.createElement('LI');
+var newLink = document.createElement('a');
+var textLink = document.createTextNode('Localhost');
+newLink.appendChild(textLink);
 newLink.setAttribute('href', 'http://localhost:3000/');
 newItem.appendChild(newLink);
 document.getElementsByClassName("navi").appendChild(newItem);
 
 // Add a new list item 
-const newFirstItem = document.createElement('li');
-newFirstItem.innerText = "Item 0";
+const newFirstItem = document.createElement('LI');
+var inText = document.createTextNode("Item 0");
+newFirstItem.appendChild(inText)
 const parentDiv = document.getElementById('ordered')
 // parentDiv.insertBefore(newFirstItem, parentDiv.firstChild);
 parentDiv.prepend(newFirstItem);
@@ -22,4 +24,4 @@ const selectedList = document.getElementById('todo');
 selectedList.classList.remove('navi');
 
 // Delete the second item from todo list
-selectedList.removeChild(selectedList.childNodes[2]);
+selectedList.removeChild(selectedList.childNodes[1]);
