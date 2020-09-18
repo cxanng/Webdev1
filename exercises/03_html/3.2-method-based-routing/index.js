990c0d305c5ccfaed85eaaaba1bd05585f9357d8
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-09-18 14:06:54 +0300

    testing ex 3.2, might be final version

---
+++ exercises/03_html/3.2-method-based-routing/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const method_res = {
+    'GET' : './get.html',
+    'POST' : './post.html'
+}
+
+const server = http.createServer((req, res) => {
+    if (Object.keys(method_res).includes(req.method)) {
+        fs.readFile(method_res[req.method], (err, data) => {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.write(data);
+            res.end();
+        });    
+    }
+    else {
+        res.writeHead(405, 'Method Not Allowed');
+        res.end();
+    }
+}).listen(3000);
