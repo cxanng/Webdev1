8278d28d029d3384bc527791314d1aef32202919
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-09-18 16:40:53 +0300

    final test ex 3.2

--- exercises/03_html/3.3-dom-tree/dom-tree.js
+++ exercises/03_html/3.3-dom-tree/dom-tree.js
@@ -1,27 +1,24 @@
 // Add a class list to the navigation class list
-const parentDiv = document.getElementByClassName("navi");
+var parentDiv = document.querySelector("nav>ul");
 parentDiv.classList.add('list');
 
 // Add a link to the nav link list
-var newItem = document.createElement('LI');
+var newListItem = document.createElement('LI');
 var newLink = document.createElement('a');
-var textLink = document.createTextNode('Localhost');
-newLink.appendChild(textLink);
 newLink.setAttribute('href', 'http://localhost:3000/');
-newItem.appendChild(newLink);
-document.getElementsByClassName("navi").appendChild(newItem);
+newLink.innerText = "Localhost"
+newListItem.appendChild(newLink);
+parentDiv.appendChild(newListItem);
 
 // Add a new list item 
 const newFirstItem = document.createElement('LI');
-var inText = document.createTextNode("Item 0");
-newFirstItem.appendChild(inText)
-const parentDiv = document.getElementById('ordered')
-// parentDiv.insertBefore(newFirstItem, parentDiv.firstChild);
-parentDiv.prepend(newFirstItem);
+newFirstItem.innerText = "Item 0";
+const orderedList = document.getElementById('ordered');
+orderedList.prepend(newFirstItem);
 
 // Remove class navi
 const selectedList = document.getElementById('todo');
 selectedList.classList.remove('navi');
 
 // Delete the second item from todo list
-selectedList.removeChild(selectedList.childNodes[1]);
+selectedList.removeChild(selectedList.childNodes[3]);
