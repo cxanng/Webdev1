2438697ad925834fb55d4ce2094ec2004ec19c03
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-09-18 20:50:42 +0300

    test ex 3.5

---
+++ exercises/03_html/3.5-html-template-tag/template.js
@@ -0,0 +1,16 @@
+const template = document.getElementById('contact-template');
+const div = document.getElementById('contacts');
+const form = document.getElementById('form');
+const submitButton = document.getElementById('submit');
+// const test = document.
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const clone = template.content.cloneNode(true);
+    const a = clone.querySelector("a");
+    clone.querySelector('h2').innerHTML = document.getElementById('input-name').value;
+    clone.querySelector('p').innerHTML = document.getElementById('input-email').value;
+    a.setAttribute('href', document.getElementById('input-homepage').value);
+    div.appendChild(clone);
+    form.reset();
+})
