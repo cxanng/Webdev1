9bd02c5bbc9d09cc10f46d593dc6035bb685cfa3
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-10-03 22:39:53 +0300

    test 1 ex 5.4

---
+++ exercises/05_javascript/5.4-messages-as-json/messages-as-json.js
@@ -0,0 +1,19 @@
+document.addEventListener('userDataReady', (event) => {
+    const content = JSON.parse(event.detail.jsonText);
+    
+    for (let object of content) {
+        const clone = document.getElementById('user-card-template').content.cloneNode(true);
+        clone.querySelector('img').setAttribute('src', object.avatar);
+        clone.querySelector('img').setAttribute('alt', object.firstName + ' ' + object.lastName);
+        clone.querySelector('h1').innerText = object.firstName + ' ' + object.lastName;
+        clone.querySelectorAll('p')[0].innerText = object.email;
+        clone.querySelectorAll('p > span').innerText = object.phoneNumber;
+        clone.querySelectorAll('div > div > p')[0].innerText = object.address.streetAddress;
+        clone.querySelectorAll('div > div > p')[1].innerText = object.address.zipCode + ' ' + object.address.city;
+        clone.querySelectorAll('div > div > p')[2].innerText = object.address.country;
+        clone.querySelectorAll('a')[0].setAttribute('href', object.homepage);
+        clone.querySelectorAll('a')[0].innerText = object.homepage;
+        document.getElementById('contacts').appendChild(clone);
+    }
+});
+fetchUserData();
