eda21b735230d064c1f1d89cc42596c8a8c3110a
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-10-03 15:36:30 +0300

    test 1 ex 5.3

--- exercises/05_javascript/5.3-events-triggered-and-emitted/custom-events.js
+++ exercises/05_javascript/5.3-events-triggered-and-emitted/custom-events.js
@@ -1,16 +1,16 @@
 const value = {
-    1: "ones",
-    2: "twos",
-    3: "threes",
-    4: "fours",
-    5: "fives",
-    6: "sixes"
+    1: ["ones", "template1"],
+    2: ["twos", "template2"],
+    3: ["threes", "template3"],
+    4: ["fours", "template4"],
+    5: ["fives", "template5"],
+    6: ["sixes", "template6"]
 }
 
 document.addEventListener('rollDice', (e) => {
     const result = e.detail.value;
     console.log(result);
-    var change = document.getElementById(value[result]).querySelector('p');
+    var change = document.getElementById(value[result][0]).querySelector('p');
     if (change.innerText === '-') {
         change.innerText = 1;
     }
@@ -19,6 +19,11 @@ document.addEventListener('rollDice', (e) => {
     }
     var counter = document.querySelector("div > p > span");
     counter.innerText = parseInt(counter.innerText) + 1;
+
+    // const clone = document.getElementById(value[result][1]).contentEditable.cloneNode(true);
+    var button = document.getElementById("roll-button");
+    button.innerHTML = document.getElementById(value[result][1]).innerHTML;
+
 });
 
 document.getElementById("roll-button").addEventListener('click', rollDice);
