c67c4f9c6550b581aaca0d1e7335121ef70d0133
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-10-03 23:15:26 +0300

    finalizing ex 5.4

--- exercises/05_javascript/5.4-messages-as-json/messages-as-json.js
+++ exercises/05_javascript/5.4-messages-as-json/messages-as-json.js
@@ -14,14 +14,14 @@ document.addEventListener('userDataReady', (event) => {
         // Modify H1
         clone.querySelector('h1').innerText = object.firstName + ' ' + object.lastName;
         
-        // Nodify contents. I have to querySelector since there was problems with getElementsByClassName.
-        clone.querySelectorAll('p')[0].innerText = object.email;
-        clone.querySelectorAll('p > span')[0].innerText = object.phoneNumber;
-        clone.querySelectorAll('div > div > p')[0].innerText = object.address.streetAddress;
-        clone.querySelectorAll('div > div > p')[1].innerText = object.address.zipCode + ' ' + object.address.city;
-        clone.querySelectorAll('div > div > p')[2].innerText = object.address.country;
-        clone.querySelectorAll('a')[0].setAttribute('href', object.homepage);
-        clone.querySelectorAll('a')[0].innerText = object.homepage;
+        // Modify contents
+        clone.querySelector('p.email').innerText = object.email;
+        clone.querySelector('p.phone > span').innerText = object.phoneNumber;
+        clone.querySelectorAll('div.address > p')[0].innerText = object.address.streetAddress;
+        clone.querySelectorAll('div.address > p')[1].innerText = object.address.zipCode + ' ' + object.address.city;
+        clone.querySelectorAll('div.address > p')[2].innerText = object.address.country;
+        clone.querySelector('p.homepage > a').setAttribute('href', object.homepage);
+        clone.querySelector('p.homepage > a').innerText = object.homepage;
 
         // Append the clone node to the contacts div
         document.getElementById('contacts').appendChild(clone);
