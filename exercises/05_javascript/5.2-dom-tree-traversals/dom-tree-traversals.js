e15cb297d55ac1fb4ede2d69a3cf05a326334468
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-10-03 23:07:23 +0300

    optimizing the algorithm for ex 5.2

--- exercises/05_javascript/5.2-dom-tree-traversals/dom-tree-traversals.js
+++ exercises/05_javascript/5.2-dom-tree-traversals/dom-tree-traversals.js
@@ -6,12 +6,7 @@ function count_child(li_element) {
     if (li_element.children.length === 0) {
         return 0;
     }
-    var result = li_element.children[0].children.length;
-    var child;
-    for (child of li_element.children[0].children) {
-        result += count_child(child)
-    }
-    return result;
+    return li_element.querySelectorAll('li').length;
 }
 
 for (li_element of li_list) {
